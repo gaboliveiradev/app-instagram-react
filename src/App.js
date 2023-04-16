6c098bce8ca7fc53e3f4835acd44f0d01beba62c
gaboliveiradev <g38789138@gmail.com>
2023-04-16 10:24:21 -0300

    utilizando props para passar dados para nosso componente Header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Header from './components/Header';
 
 function App() {
   /*
+  * Depois que o usuário logar, iremos armazenar dentro de user, para futuramente sabermos qual tela exibir para ele
+  * a tela de login, ou caso ele esteja logado, exibiremos outra tela.
+  */
+  const [user, setUser] = useState("Gabriel");
+
+  /*
    * Como se fosse um constructor do php, c#. Ou seja é chamado quando nosso 
    * aplicativo é montado/inicializado.
    */
@@ -13,9 +19,15 @@ function App() {
     
   },[])
 
+
+  /* 
+   * Passando os dados da useState via props para nosso componente header
+   * Props: é um estágio do desenvolvimento que acontece quando precisamos obter dados que estão 
+   * em várias camadas na árvore de componente react
+   */
   return (
     <div className="App">
-      <Header></Header>
+      <Header user={user}></Header>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,6 @@
 import { useEffect, useState } from 'react';
 
-function Header() {
-    /*
-    * Depois que o usuário logar, iremos armazenar dentro de user, para futuramente sabermos qual tela exibir para ele
-    * a tela de login, ou caso ele esteja logado, exibiremos outra tela.
-    */
-    const [user, setUser] = useState("Gabriel");
-
+function Header(props) {
     return (
         <><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css"></link><div className="header">
             <div className="header__logo">
@@ -15,9 +9,9 @@ function Header() {
                 </a>
             </div> {/* Fechamento Div Header__Logo */}
 
-            {(user) ?
+            {(props.user) ?
                 <div className="header__info__logado">
-                    <p><i class="bi bi-person-circle"></i> Olá, <b>{user}</b></p>
+                    <p><i class="bi bi-person-circle"></i> Olá, <b>{props.user}</b></p>
                     <a href="#">Fazer Publicação</a>
                 </div>
                 :
