c4f69b9caf5673378a56fcd6ff055b88a4bd732c
gaboliveiradev <g38789138@gmail.com>
2023-04-20 08:55:23 -0300

    criando regras de segurança (exibindo os comentários e permitindo comentar apenas quem está logado + sistema de comentários finalizado

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,6 +4,19 @@ import { useEffect, useState } from 'react';
 
 function Feed(props) {
 
+    const [comentarios, setComentarios] = useState([]);
+
+    useEffect(()=>{
+        db.collection('posts').doc(props.id).collection('comentarios').onSnapshot((snapshot)=>{
+          setComentarios(snapshot.docs.map((document)=>{
+            return {
+              id: document.id,
+              info: document.data()
+            }
+          }))
+        });
+    },[])
+
     function comentar(id, e) {
         e.preventDefault();
 
@@ -34,12 +47,33 @@ function Feed(props) {
               <div className="post__descricao">
                 <p>{props.info.descricao}</p>
               </div>
-              <div className="post__comentar">
-                <form onSubmit={(e)=>comentar(props.id, e)}>
-                  <input id={"comentario-"+props.id} type="text" placeholder="escreva um comentário" />
-                  <input type="submit" name="bnt-comentar" value="Comentar" />
-                </form>
-              </div>
+              { (props.user == null) ?
+                <div className="post__comentar">
+                    <p>Você precisa ter uma conta e estar logado para comentar em uma publicação e ver os comentários existentes.</p>
+                </div>
+                :
+                <div className="post__comentar">
+                    <form onSubmit={(e)=>comentar(props.id, e)}>
+                      <input id={"comentario-"+props.id} type="text" placeholder="escreva um comentário" />
+                      <input type="submit" name="bnt-comentar" value="Comentar" />
+                    </form>
+                </div>
+              }
+              { (props.user) ?
+                <div className="post__comentarios">
+                  {
+                      comentarios.map((val)=>{
+                          return (
+                              <div className="comments__single">
+                                  <p><i class="bi bi-person-hearts"></i> <b>{val.info.nome}</b> {val.info.comentario}</p>
+                              </div>
+                          );
+                      })
+                  }
+                </div>
+                :
+                ""
+              }
             </div>
         </div>
     );
